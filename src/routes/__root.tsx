cdf0045b3d22c7322935276adb24936e19647393
Cory House <housecor@gmail.com>
2026-05-19 14:45:05 -0500

    add

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,24 @@
+import { Button } from "@/components/ui/button";
 import { useUser } from "@/UserContext";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => {
-    const { email } = useUser();
+    const { email, setEmail } = useUser();
     return (
       <>
         <nav>
-          <Link to="/">Home</Link> | <Link to="/admin">Admin</Link> |{" "}
+          <Link to="/">Home</Link> |{" "}
+          {email && (
+            <>
+              <Link to="/admin">Admin</Link> |
+            </>
+          )}{" "}
           {email ? (
-            <span>Welcome, {email}</span>
+            <>
+              <span>Welcome, {email}</span>{" "}
+              <Button onClick={() => setEmail("")}>Log out</Button>
+            </>
           ) : (
             <Link to="/login">Login</Link>
           )}
