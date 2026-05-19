444c446f525029a35def0b56069f19d868e40d22
Cory House <housecor@gmail.com>
2026-05-19 11:07:27 -0500

    add

--- src/shared/Input.tsx
+++ src/shared/Input.tsx
@@ -1,13 +1,23 @@
 type InputProps = {
-    label: string;
-    name: string;
-    value: string;
-    onChange: (value: string) => void;
-}
+  label: string;
+  name: string;
+  value: string;
+  onChange: (value: string) => void;
+};
 
 export default function Input(props: InputProps) {
-    return <div className="p-2">
-        <label htmlFor={props.name} className="block">{props.label}</label>
-        <input  id={props.name} value={props.value} onChange={(e) => props.onChange(e.target.value)} className="border p-2 rounded" type="text" />
+  return (
+    <div className="p-2">
+      <label htmlFor={props.name} className="block">
+        {props.label}
+      </label>
+      <input
+        id={props.name}
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+        className="border p-2 rounded"
+        type="text"
+      />
     </div>
+  );
 }
