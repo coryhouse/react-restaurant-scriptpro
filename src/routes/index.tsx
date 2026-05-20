4f18904b5ad3165bc7e06c8402f1076a696a4c6f
Cory House <housecor@gmail.com>
2026-05-20 13:05:05 -0500

    use url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
+import z from "zod";
+import { foodTags } from "../food";
 import Menu from "../Menu";
+
+export const menuSearchSchema = z.object({
+  q: z.string().optional(),
+  tag: z.enum(foodTags).optional(),
+  minPrice: z.number().min(0).max(25).optional(),
+  maxPrice: z.number().min(0).max(25).optional(),
+});
+
+export type MenuSearch = z.infer<typeof menuSearchSchema>;
 
 export const Route = createFileRoute("/")({
   component: Menu,
+  validateSearch: menuSearchSchema,
 });
